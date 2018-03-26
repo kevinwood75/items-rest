f2446dcd92f90eb0e34bd66dcb101ef40383ebf3
kevinwood75 <kevin.wood75@gmail.com>
2018-03-25 22:59:49 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'woodez'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity) # creates a new endpoint called /auth
+
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(Item, '/item/<string:name>') # http://127.0.0.1:5000/student/Rolf
+api.add_resource(ItemList, '/items')
+api.add_resource(StoreList, '/stores')
+api.add_resource(UserRegister, '/register')
+
+
+if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
+    app.run(port=5000, debug=True)
